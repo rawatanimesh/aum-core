2eb87570998f332ae241b4e6573941ba1d0f42fd
Animesh Rawat <animesh.rawat20@gmail.com>
2025-07-17 23:35:18 +0530

    feat: added playgrond module and rem() scss function

---
+++ libs/modules/playground/src/lib/playground/playground.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'aum-playground',
+  imports: [CommonModule],
+  templateUrl: './playground.html',
+  styleUrl: './playground.scss',
+})
+export class Playground {}
